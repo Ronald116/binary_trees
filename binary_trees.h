695c596f7aec6c969e3635f3a9cccc76edf570ae
Ronald116 <ronaldbotchway@gmail.com>
2023-08-04 07:38:49 -0700

    task 123

--- binary_trees.h
+++ binary_trees.h
@@ -59,6 +59,10 @@ int height(const binary_tree_t *tree);
 int is_bst(const binary_tree_t *tree, int min, int max);
 int is_avl_tree(const binary_tree_t *tree);
 avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval);
+bst_t *bst_remove(bst_t *root, int value);
+int remove_type(bst_t *root);
+int successor(bst_t *node);
+void bal(avl_t **tree);
 
 /*Prototype functions*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
